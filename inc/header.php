1dafec81630419ea8d80a5ad246f30aa64e37639
Ernesto Aparicio <ernesto@ultra.me>
2014-09-26 23:37:56 -0700

    Added header, index.php

---
+++ inc/header.php
@@ -0,0 +1,15 @@
+<header role="banner">
+    <nav role="navigation" class="navbar navbar-static-top navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Project name</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.html">Home</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -22,21 +22,7 @@
         <![endif]-->
 
         <!-- Add your site or application content here -->
-        <header role="banner">
-            <nav role="navigation" class="navbar navbar-static-top navbar-default">
-                <div class="container">
-                    <div class="navbar-header">
-                        <a class="navbar-brand" href="#">Project name</a>
-                    </div>
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="index.html">Home</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+        <?php include 'inc/header.php'; ?>
 
         <main role="main">
             <h1>Main Heading</h1>
